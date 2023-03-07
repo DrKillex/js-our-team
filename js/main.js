6f5a4cc0befece20f2f4f2d1448a8a2cf7ec5b2d
DrKillex <lcollaro1994@gmail.com>
2023-03-07 17:12:52 +0100

    completo no commenti

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 'use strict';
 
+// funzioni ----------------------------------------------------------------------------------------
+
+function myColCreator (){
+    const createdElement = document.createElement('div');
+    createdElement.classList.add('col-4', 'mb-2');
+    return createdElement
+};
+
+function myCardCreator (){
+    const createdCard = document.createElement('div');
+    createdCard.classList.add('card');
+    return createdCard
+};
+
+function myCardBodyCreator (){
+    const createdCardBody = document.createElement('div');
+    createdCardBody.classList.add('card-body');
+    return createdCardBody
+};
+
+function myImgCreator (){
+    const createdImg = document.createElement('img');
+    createdImg.classList.add('card-img-top');
+    return createdImg
+};
+
+function myCardTitleCreator (){
+    const createdCardTitle = document.createElement('h5');
+    createdCardTitle.classList.add('card-title');
+    return createdCardTitle
+};
+
+function myCardTxtCreator (){
+    const createdCardTxt = document.createElement('p');
+    createdCardTxt.classList.add('card-text');
+    return createdCardTxt
+};
+
+// main ----------------------------------------------------------------------------------------
+
 const team = [
     {
         nome: 'Wayne Barnett',
@@ -33,10 +73,36 @@ const team = [
     }
 ];
 
+const appendSelector = document.querySelector('.row.justify-content-between');
+
 for (let i = 0; i < team.length; i++){
     let currentTeamMember = team[i];
+    const createdElement = myColCreator();
+    const createdCard = myCardCreator();
+    const createdCardBody = myCardBodyCreator();
+    const createdCardTxt = myCardTxtCreator();
     console.log(currentTeamMember);
     for (let key in currentTeamMember){
-        console.log(currentTeamMember[key])
+        console.log(currentTeamMember[key]);
+        if (key === 'img'){
+            const createdImg = myImgCreator()
+            createdImg.src = `img/${currentTeamMember[key]}`
+            createdCard.append(createdImg);
+        } else if (key === 'nome'){
+            const createdCardTitle = myCardTitleCreator();
+            createdCardTitle.innerText = currentTeamMember[key]
+            createdCardBody.append(createdCardTitle);
+        } else if (key === 'ruolo'){
+            const createdCardTxt = myCardTxtCreator();
+            createdCardTxt.innerText = currentTeamMember[key]
+            createdCardBody.append(createdCardTxt);
+        }
     }
-}
+    appendSelector.append(createdElement);
+    createdElement.append(createdCard);
+    createdCard.append(createdCardBody);
+    createdCardBody.append(createdCardTxt);
+};
+
+
+
